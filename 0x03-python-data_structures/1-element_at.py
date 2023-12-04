881865caf595ede58e744d9ab5665ebc36ce5259
Houda0612 <138779036+Houda0612@users.noreply.github.com>
2023-12-04 21:15:37 +0100

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-  """
-    Gets an element in a list at given index
-    And returns it
-    """
     my_list_len = len(my_list)
     if idx < 0 or idx > my_list_len - 1:
         return 'None'
